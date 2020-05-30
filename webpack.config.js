8715158dc15277bd679d972a56f92e5469930ec6
Rakesh M R <rakeshmr723@gmail.com>
2020-05-30 16:15:06 +0530

    used webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  entry: '/home/rakesh/Rakesh-m-r/webdev/TechVidya-2k19/script/script.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    publicPath: '/dist/',
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname,'node_modules')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+};
