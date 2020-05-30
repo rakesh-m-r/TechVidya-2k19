dbc49b45066ab0e5805388935a3b50797d1819fb
=rakesh-m-r <rakeshmr723@gmail.com>
2020-05-30 17:40:47 +0530

    used webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
 module.exports = {
+  mode: 'production',
   entry: '/home/rakesh/Rakesh-m-r/webdev/TechVidya-2k19/script/script.js',
   output: {
-    path: path.resolve(__dirname,'dist'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: "bundle.js",
   },
   module: {
     rules: [{
         test: /\.js$/,
-        exclude: [path.resolve(__dirname,'node_modules')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
         loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ]
   },
 };
